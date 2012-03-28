7caf97c3b3189a073a1bb97567fd6c16864da473
KalimochoAz <calimochoazucarado@gmail.com>
2012-03-28 18:06:20 +0200

    Change Wi-Fi param name to meet with Franco kernel

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -317,10 +317,10 @@ module_param(dhd_console_ms, uint, 0644);
 #endif /* defined(DHD_DEBUG) */
 
 /* Controls Status off wiffi Power on SleepMode
-   sys/module/bcmdhd/parameters/uiFastWifi      */
+   sys/module/bcmdhd/parameters/wifi_pm      */
 #if defined(CONFIG_HAS_EARLYSUSPEND)
-uint uiFastWifi = 1;
-module_param(uiFastWifi, uint, 0664);
+uint wifi_pm = 1;
+module_param(wifi_pm, uint, 0664);
 #endif /* defined(CONFIG_HAS_EARLYSUSPEND) */
 
 /* ARP offload agent mode : Enable ARP Host Auto-Reply and ARP Peer Auto-Reply */
@@ -530,8 +530,8 @@ static int dhd_set_suspend(int value, dhd_pub_t *dhd)
 	int bcn_li_dtim = 3;
 	uint roamvar = 1;
 
-    if (uiFastWifi == 1)
-        power_mode = PM_FAST;
+	if (wifi_pm == 1)
+		power_mode = PM_FAST;
 
 	DHD_TRACE(("%s: enter, value = %d in_suspend=%d\n",
 		__FUNCTION__, value, dhd->in_suspend));
