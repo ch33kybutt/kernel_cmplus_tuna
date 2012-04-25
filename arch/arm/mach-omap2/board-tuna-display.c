09348312dc4626ae86ab89b5e31e87fbdcb422ee
ch33kybutt <ch33kybutt@yahoo.com>
2012-04-25 21:46:18 +0800

    Revert "Customize default colour multipliers"
    This reverts commit b46cee35cf73bd0379b4e3ff3326205d922fa357.

--- arch/arm/mach-omap2/board-tuna-display.c
+++ arch/arm/mach-omap2/board-tuna-display.c
@@ -840,9 +840,9 @@ static struct s6e8aa0_factory_calibration_info tuna_oled_factory_info_6500k = {
 		 * maximum brightness down from 300 cd/m2 to 280.
 		 */
 		.mult = {
-			1400000000U,
-			1700000000U,
-			2100000000U,
+			2004318071U,
+			2004318071U,
+			2004318071U,
 		},
 		.rshift = 31,
 	},
@@ -892,9 +892,9 @@ static struct s6e8aa0_factory_calibration_info tuna_oled_factory_info_sm2a2 = {
 		 * maximum brightness down from 300 cd/m2 to 280.
 		 */
 		.mult = {
-			1400000000U,
-			1700000000U,
-			2100000000U,
+			2004318071U,
+			2004318071U,
+			2004318071U,
 		},
 		.rshift = 31,
 	},
